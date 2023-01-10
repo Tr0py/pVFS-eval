c830f857504c5612d2695eb25f11d2fb1f9612d7
ziyi <zz89@rice.edu>
2023-01-10 14:22:25 -0600

    SCRIPT: update perf graph script.

--- scripts/perf/perf-graph.sh
+++ scripts/perf/perf-graph.sh
@@ -3,14 +3,18 @@
 # Set FLAME_GRAPH_ROOT  to the root of the FlameGraph directory
 # Output: ./perf-data/<name>.svg containing the FlameGraph
 
-PERF=perf
+#Please run as root!
+
+set -x
+
+PERF=~/perf
 FLAME_GRAPH_ROOT=/home/ziyi/git/pvfs-eval/scripts/FlameGraph
 
 mkdir perf-data
 
 # Use perf to get data
-sudo $PERF record -F 99  -v  -g --  $2 $3 $4 $5 $6 $7   #generate perf data
-sudo $PERF script > ./perf-data/out.$1   # generate stack data
+time $PERF record -F 99 --call-graph fp -g  $2 $3 $4 $5 $6 $7   #generate perf data
+$PERF script > ./perf-data/out.$1   # generate stack data
 
 # Use FlameGraph to generate graph
 $FLAME_GRAPH_ROOT/stackcollapse-perf.pl ./perf-data/out.$1 > ./perf-data/$1.fold # fold data
